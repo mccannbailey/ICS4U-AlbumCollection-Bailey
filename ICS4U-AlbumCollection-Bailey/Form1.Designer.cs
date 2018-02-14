197fa74f2335c00644a9b9eb9e27c008069d6296
Bailey McCann <mccannbailey@hotmail.com>
2018-02-14 16:53:39 -0500

    Add project files.

---
+++ ICS4U-AlbumCollection-Bailey/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace ICS4U_AlbumCollection_Bailey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.origBox = new System.Windows.Forms.RichTextBox();
+            this.sortBox = new System.Windows.Forms.RichTextBox();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // origBox
+            // 
+            this.origBox.Enabled = false;
+            this.origBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.origBox.Location = new System.Drawing.Point(12, 144);
+            this.origBox.Name = "origBox";
+            this.origBox.Size = new System.Drawing.Size(275, 300);
+            this.origBox.TabIndex = 1;
+            this.origBox.Text = "";
+            // 
+            // sortBox
+            // 
+            this.sortBox.Enabled = false;
+            this.sortBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sortBox.Location = new System.Drawing.Point(297, 144);
+            this.sortBox.Name = "sortBox";
+            this.sortBox.Size = new System.Drawing.Size(275, 300);
+            this.sortBox.TabIndex = 2;
+            this.sortBox.Text = "";
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabel.ForeColor = System.Drawing.Color.White;
+            this.infoLabel.Location = new System.Drawing.Point(12, 57);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(175, 24);
+            this.infoLabel.TabIndex = 3;
+            this.infoLabel.Text = "Enter Information:";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(194, 60);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(235, 20);
+            this.inputBox.TabIndex = 4;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(453, 43);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 5;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(453, 72);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.TabIndex = 6;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.ForeColor = System.Drawing.Color.Red;
+            this.statusLabel.Location = new System.Drawing.Point(13, 9);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 18);
+            this.statusLabel.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(9, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 18);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Original Order:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(294, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Sorted Order:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(502, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Bailey McCann";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.sortBox);
+            this.Controls.Add(this.origBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Collection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox origBox;
+        private System.Windows.Forms.RichTextBox sortBox;
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ICS4U-AlbumCollection-Bailey/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ICS4U_AlbumCollection_Bailey
+{
+    public partial class Form1 : Form
+    {
+        List<string> origList = new List<string>(); // original list and sorted list
+        List<string> sortList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string info;
+            statusLabel.Text = "";
+            statusLabel.ForeColor = Color.Red;
+
+            try
+            {
+                if (inputBox.Text != "")
+                {
+                    info = inputBox.Text;
+
+                    origList.Add(info);
+                    sortList.Add(info);
+                    sortList.Sort();
+
+                    statusLabel.ForeColor = Color.Green;
+                    statusLabel.Text = "Success!";
+                }
+                else if (inputBox.Text == "") { statusLabel.Text = "Please enter information."; }
+            }   
+            catch { statusLabel.Text = "An error occured"; }
+
+            var origOrder = string.Join("\n", origList.ToArray());
+            origBox.Text = origOrder;
+
+            var sortOrder = string.Join("\n", sortList.ToArray());
+            sortBox.Text = sortOrder;
+
+            inputBox.Clear();
+            inputBox.Focus();
+
+            this.Refresh();
+            Thread.Sleep(1000);
+            statusLabel.Text = "";
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {           
+            string info;
+            info = inputBox.Text;
+
+            if (origList.Contains(info) || sortList.Contains(info))
+            {
+                origList.Remove(info);
+                sortList.Remove(info);
+
+                statusLabel.ForeColor = Color.Green;
+                statusLabel.Text = "Successfully removed!";
+
+                var origOrder = string.Join("\n", origList.ToArray());
+                origBox.Text = origOrder;
+
+                var sortOrder = string.Join("\n", sortList.ToArray());
+                sortBox.Text = sortOrder;
+                
+                inputBox.Clear();
+                inputBox.Focus();
+            }
+            else
+            {
+                statusLabel.ForeColor = Color.Red;
+                statusLabel.Text = "Entered information does not exist.";
+            }
+
+            this.Refresh();
+            Thread.Sleep(1000);
+            statusLabel.Text = "";
+        }
+    }
+}
